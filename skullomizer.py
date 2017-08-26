d1a177cd1f083bb65b1f09723d43d90d54223b01
pseudophpt <30391650+pseudophpt@users.noreply.github.com>
2017-08-26 18:24:20 -0400

    Update skullomizer.py

--- skullomizer.py
+++ skullomizer.py
@@ -7,11 +7,20 @@ current_skulls = [0,0,0,0,0]
 number_changes = [0,0,0,0,0]
 amt_skulls = 0
 bonus_amt = 0
-bonus_times = 0
+
+def gen_skull( ):
+	x = 0
+	while x == 0: 
+		skull_no = randint(0, 99)
+		if used_skulls[skull_no] == 0:
+			x = 1
+	return skull_no
 
 # Set seed
 seed = raw_input('Seed: ')
 random.seed(seed)
+
+flash = raw_input('Flash mode? (y/n):')
 
 bonus = raw_input('Bonus points for all 5 finished? (y/n):')
 if bonus == "y":
@@ -29,7 +38,8 @@ for skull in range (0,5):
 for skull in range (0,5):
 
 	# Print current skulls
-	textline = linecache.getline("skulls.txt", current_skulls[skull])
+	#textline = linecache.getline("skulls.txt", current_skulls[skull])
+	textline = linecache.getline("skulls.txt", current_skulls[skull] + 1)
 	print(str(skull + 1) + ". "+textline)
 
 while 1:
@@ -40,30 +50,37 @@ while 1:
 	amt_skulls += 1
 
 	# Check for bonuses
-	number_changes[number_int] += 1
+	number_changes[number_int] = 1
 	num_bonuses = 0
 	for skull in range (0,5):
-		if number_changes[skull] > bonus_times:
+		if number_changes[skull] == 1:
 			num_bonuses += 1
 	if (num_bonuses == 5) & (bonus_amt > 0):
-		bonus_times += 1
 		amt_skulls += bonus_amt
 		print(str(bonus_amt) + " point bonus!")
-		
-	print("Points: " + str(amt_skulls))
+		for skull in range (0,5):
+			number_changes[skull] = 0
 
-	# Generate random numbers until a valid uncompleted skull is found
-	x = 0
-	while x == 0: 
-		skull_no = randint(0, 99)
-		if used_skulls[skull_no] == 0:
-			x = 1
+	# Print amount of points
+	print("Points: " + str(amt_skulls))
+	
+	if flash == "y":
+		for skull in range(0,5):
+			skull_no = gen_skull()
+			used_skulls[current_skulls[number_int]] = 1
+			current_skulls[skull] = skull_no
+	else:
+		skull_no = gen_skull()
+		used_skulls[skull_no] = 1
+		current_skulls[number_int] = gen_skull()
 
 	# Set it as used and add it to the current 5 to replace the last gotten skull
-	used_skulls[skull_no] = 1
+	
 	current_skulls[number_int] = skull_no
 
 	# Print current skulls
 	for skull in range (0,5):
-		textline = linecache.getline("skulls.txt", current_skulls[skull])
+		textline = linecache.getline("skulls.txt", current_skulls[skull] + 1)
 		print(str(skull+1) + ". "+textline)
+
+
